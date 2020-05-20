218f69b83972008a32866348992514853c5b9de3
chocolatejade42 <Aiden.Gardner@icloud.com>
2020-05-20 17:03:17 +1000

    Add buzzer play on message It's blocking but it works so here

--- project/project.ino
+++ project/project.ino
@@ -8,6 +8,7 @@
 
 // Imports
 #include "config.h"
+#include "pitches.h"
 #include "AdafruitIO_WiFi.h"
 
 // Set up feeds
@@ -80,4 +81,25 @@ void handleInput(AdafruitIO_Data *data) {
 
     // Write input to LED
     digitalWrite(LED_PIN, data -> toPinLevel());
+    if (PLAY_TUNE) {
+        playTune();
+    }
+}
+
+void playTune() {
+    // Melody
+    int melody[] = {NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
+
+    // Note durations
+    int noteDurations[] = {
+        4, 8, 8, 4, 4, 4, 4, 4
+    };
+
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
+        int noteDuration = 1000 / noteDurations[thisNote];
+        tone(PIEZO_PIN, melody[thisNote], noteDuration);
+        int pauseBetweenNotes = noteDuration * 1.30;
+        delay(pauseBetweenNotes);
+        noTone(PIEZO_PIN);
+    }
 }
